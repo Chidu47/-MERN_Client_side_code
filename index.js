ecafcf330ec50a33e0c9ced9fc53ba981deae3cc
Chidu47 <94058157+Chidu47@users.noreply.github.com>
2021-12-31 12:54:22 +0530

    make it better

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-// app.get('/', (req, res) => {
-//     res.send('Hello to Memories API');
-// });
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
 
 //connecting to database MongoDB
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 // const URL = process.env.CONNECTION_URL
 
 mongoose.connect(process.env.CONNECTION_URL)
