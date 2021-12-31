d7b264ca42b8ac793254ff29e7e62ce1d34de198
Chidu47 <94058157+Chidu47@users.noreply.github.com>
2021-12-31 12:25:46 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,33 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
+const app = express();
+
+dotenv.config();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
+
+//connecting to database MongoDB
+const PORT = process.env.PORT || 5000;
+// const URL = process.env.CONNECTION_URL
+
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`))) 
+    .catch((error) => console.error(error.message))
+    
